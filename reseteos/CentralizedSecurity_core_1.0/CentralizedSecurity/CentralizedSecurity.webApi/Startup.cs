535415d8ebed2c866c2461cf5074197a37a373c3
moviedomfo2 <marcelo.oviedo@gmail.com>
2019-09-16 10:59:24 -0300

    start up jwt config

--- reseteos/CentralizedSecurity_core_1.0/CentralizedSecurity/CentralizedSecurity.webApi/Startup.cs
+++ reseteos/CentralizedSecurity_core_1.0/CentralizedSecurity/CentralizedSecurity.webApi/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using CentralizedSecurity.webApi.common;
 using CentralizedSecurity.webApi.helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CentralizedSecurity.webApi
 {
@@ -34,38 +37,71 @@ namespace CentralizedSecurity.webApi
 
             //services.a(  // enable Configuration Services
 
-            var config = new serverSettings();
-            Configuration.Bind("serverSettings", config);      //  <--- This
-            services.AddSingleton(config);
+            var appSettings = new serverSettings();
+            Configuration.Bind("serverSettings", appSettings);      //  <--- This
+            services.AddSingleton(appSettings);
             //https://weblog.west-wind.com/posts/2017/dec/12/easy-configuration-binding-in-aspnet-core-revisited
-            apiAppSettings.serverSettings = config;
+            apiAppSettings.serverSettings = appSettings;
 
 
             // configure DI for application services
             services.AddScoped<IMeucciService, MeucciService>();
             services.AddScoped<ILDAPService, LDAPService>();
+
+
+
+            #region configure jwt authentication
+            //  var appSettings = appSettingsSection.Get<serverSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.apiConfig.api_secretKey);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseHsts();
+            //}
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
+
             app.UseMvc();
 
 
-           
 
-          
-          
+
+
+
         }
     }
 }
