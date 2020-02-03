3b9116edf8468e6d624641ac922223c98f4230b2
moviedomfo2 <marcelo.oviedo@gmail.com>
2020-02-03 18:14:57 -0300

    let appInstance = this.authService.getAppInstance();

--- EpironWeb/EpironFront/src/app/service/epiron.security.service.ts
+++ EpironWeb/EpironFront/src/app/service/epiron.security.service.ts
@@ -1,12 +1,12 @@
 
-import {  Result, EpironApiResponse } from '../model/common.model';
+import { Result, EpironApiResponse } from '../model/common.model';
 import { AppConstants } from "../model/common.constants";
 import { HttpClient } from '@angular/common/http';
-import {  Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { CommonService } from './common.service';
 import { Injectable } from '@angular/core';
-import {  ValidarAplicacionRes } from '../model/epiron/epiron.security.model';
+import { ValidarAplicacionRes } from '../model/epiron/epiron.security.model';
 import { helperFunctions } from './helperFunctions';
 import { CurrentLoginEpiron, UserAutenticacionRes } from '../model';
 import { AuthenticationService } from './authentication.service';
@@ -14,27 +14,27 @@ import { AuthenticationService } from './authentication.service';
 
 @Injectable()
 export class EpironSecurityService {
-    
-  constructor(private http: HttpClient, private commonService: CommonService,private authService:AuthenticationService ) {
-  
+
+  constructor(private http: HttpClient, private commonService: CommonService, private authService: AuthenticationService) {
+
   }
 
 
   public userAutenticacion$(userName: string, password: string, domain: string, returnUrl: string): Observable<any> {
 
-    
+
     //UserAutenticacionReq
     var bussinesData = {
-      event_tag : 'USER-AUTENTIC',
-      appInstanceGUID : AppConstants.AppInstanceGUID,
-      LoginHost:this.commonService.get_host(),
+      event_tag: 'USER-AUTENTIC',
+      appInstanceGUID: AppConstants.AppInstanceGUID,
+      LoginHost: this.commonService.get_host(),
       LoginIP: this.commonService.ipinfo.ip,
       userName: userName,
       userKey: password,
       domainGUID: 'FDEB4B1F-229E-E311-9DD1-0022640637C2', //domain allus-ar,
       AutTypeGUID: '71C15455-D147-E311-A348-000C292448BD',
-      guidintercambio :'75CFEFE4-5A79-E411-BD73-0022640637C2',
-      UserKey : password,
+      guidintercambio: '75CFEFE4-5A79-E411-BD73-0022640637C2',
+      UserKey: password,
 
       //grant_type: 'password',
       //client_id: AppConstants.oaut_client_id,
@@ -48,14 +48,14 @@ export class EpironSecurityService {
 
           let currentLogin: CurrentLoginEpiron = new CurrentLoginEpiron();
 
-          if(res.Errors){
-              alert(JSON.stringify);
+          if (res.Errors) {
+            alert(JSON.stringify);
           }
           currentLogin.oAuthResult = new UserAutenticacionRes();
           currentLogin.oAuthResult = res.Result as UserAutenticacionRes;
 
-          if(!res.Errors){
-              helperFunctions.handleEpironError(res.Errors);
+          if (!res.Errors) {
+            helperFunctions.handleEpironError(res.Errors);
           }
           // let tokenInfo = jwt_decode(currentLogin.oAuthResult.access_token); // decode token
 
@@ -66,7 +66,7 @@ export class EpironSecurityService {
           // currentLogin.currentUser.email = tokenInfo.email;
           // currentLogin.currentUser.roles = tokenInfo.roles;
           // currentLogin.currentUser.personId = tokenInfo.personId;
-      
+
           localStorage.setItem('currentLogin', JSON.stringify(currentLogin));
           // this.getUser_Data$(currentLogin.currentUser.personId, '').subscribe(res => {
 
@@ -88,47 +88,36 @@ export class EpironSecurityService {
 
   }
 
-  
+
   public validateAppService$(): Observable<any> {
-    
-    
+
+
     var bussinesData = {
-        AppInstanceGuid: AppConstants.AppInstanceGUID,
-        LoginHost: this.commonService.get_host(),
-        LoginIp: this.commonService.ipinfo.ip
-        
-    
+      AppInstanceGuid: AppConstants.AppInstanceGUID,
+      LoginHost: this.commonService.get_host(),
+      LoginIp: this.commonService.ipinfo.ip
+
+
     };
 
- 
+
     return this.http.post<EpironApiResponse>(`${AppConstants.AppAPI_URL}security/validarAplicacion`,
       bussinesData, AppConstants.httpClientOption_contenttype_json).pipe(
         map(res => {
 
-          
-
-          if(res.Errors){
-              alert(JSON.stringify);
+          if (res.Errors) {
+            helperFunctions.handleEpironError(res.Errors);
           }
-          
-          let result= res.Result as ValidarAplicacionRes;
 
-          if(!res.Errors){
-              helperFunctions.handleEpironError(res.Errors);
-          }
-     
-      
-          localStorage.setItem('allInstance', JSON.stringify(result));
-          let appInst =  this.authService.getAppInstance();
+          let appInstance = res.Result as ValidarAplicacionRes;
+          localStorage.setItem('appInstance', JSON.stringify(appInstance));
+         
+         
 
-            alert(JSON.stringify(appInst));
-
-          return appInst;
+          return appInstance;
         })).pipe(catchError(helperFunctions.handleError));
 
 
-
-
-}
+  }
 }
 
